8d59c824556490feddf0ecc317e5f3403e4c5d03
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-05-09 00:34:33 +0700

    Create User model

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
 const app = express();
 
 const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.send('👋 Chao Xin'))
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);

d937a698a6f2edcb39c54fee1e9d06cf812ea481
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-05-11 23:57:23 +0700

    Validate input

---
+++ server/validation/register.js
@@ -0,0 +1,52 @@
+const validator = require('validator');
+
+const validateRegisterInput = data => {
+  const errors = {};
+  data.name = data.name || '';
+  data.email = data.email || '';
+  data.password = data.password || '';
+  data.confirmPassword = data.confirmPassword || '';
+
+  if (!validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = 'Name must be between 2 to 30 chars';
+  }
+
+  if (validator.isEmpty(data.name)) {
+    errors.name = 'Name is required';
+  }
+
+  if (!validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if (validator.isEmpty(data.email)) {
+    errors.email = 'Email is required';
+  }
+
+  if (!validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = 'Password must be between 6 to 30 chars';
+  }
+
+  if (validator.isEmpty(data.password)) {
+    errors.password = 'Password is required';
+  }
+
+  if (!validator.isLength(data.confirmPassword, { min: 6, max: 30 })) {
+    errors.confirmPassword = 'Password must be between 6 to 30 chars';
+  }
+
+  if (validator.isEmpty(data.password)) {
+    errors.confirmPassword = 'Password is required';
+  }
+
+  if (!validator.equals(data.password, data.confirmPassword)) {
+    errors.confirmPassword = 'Password and confirm password must match';
+  }
+
+  return {
+    errors,
+    isValid: Boolean(errors)
+  };
+};
+
+module.exports = validateRegisterInput;
---
+++ server/validations/login.js
@@ -0,0 +1,30 @@
+const validator = require('validator');
+
+const validateLoginInput = data => {
+  const errors = {};
+  data.email = data.email || '';
+  data.password = data.password || '';
+
+  if (!validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if (validator.isEmpty(data.email)) {
+    errors.email = 'Email is required';
+  }
+
+  if (!validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = 'Password must be between 6 to 30 chars';
+  }
+
+  if (validator.isEmpty(data.password)) {
+    errors.password = 'Password is required';
+  }
+
+  return {
+    errors,
+    isValid: Boolean(errors)
+  };
+}
+
+module.exports = validateLoginInput;

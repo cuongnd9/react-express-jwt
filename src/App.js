7ad033468cfa476a65bca318ff1862fe646e3a21
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-05-14 23:57:47 +0700

    Check the logged in user status

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import jwtDecode from 'jwt-decode';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { setCurrentUser, logoutUser } from './actions/authentication';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, Navbar, Register, Login } from './components';
+import store from './store';
 
 function App() {
+  useEffect(() => {
+    const jwtToken = localStorage.getItem('jwtToken');
+    if (jwtToken) {
+      const decoded = jwtDecode(jwtToken);
+      store.dispatch(setCurrentUser(decoded));
+
+      const currentTime = Date.now() / 1000;
+      if (decoded.exp < currentTime) {
+        store.dispatch(logoutUser());
+        window.location.href = '/login';
+      }
+    }
+  });
+
   return (
     <Router>
       <Navbar />
